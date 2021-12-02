ee546b23601e286b2675558d5922ea26205c7a86
tkozlovets <tkozlovets@ukr.net>
2021-12-02 18:33:45 +0200

    added task2

--- lesson5/js/myhomework.js
+++ lesson5/js/myhomework.js
@@ -1,7 +1,7 @@
 // Написать функцию которая первым принимает значение, которым заполнять массив, а вторым - сколько элементов
 //  должно быть в массиве. Пример: arrayFill('x', 5) сделает массив ['x', 'x', 'x', 'x', 'x'].
 
-let arr = []
+// let arr = []
 
 // function fillArray(a, b)
 // {
@@ -11,13 +11,29 @@ let arr = []
 //    return arr.fill(a, 0, b)
 // }
 // }
+// let fillArray = (a, b) => 
+// {
+//     for (i=0; i < b; i++) {
+//         arr[i]=a;
+//         arr.length = b;
+//    return arr.fill(a, 0, b)
+// }
+// }
+// console.log(fillArray(1, 9))
 
-let fillArray = (a, b) => 
-{
-    for (i=0; i < b; i++) {
-        arr[i]=a;
-        arr.length = b;
-   return arr.fill(a, 0, b)
-}
-}
-console.log(fillArray(1, 9))
+// task 2
+// Дан трехмерный массив с числами, например [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. 
+// Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
+
+// let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+
+// const arr1 = arr.flat()
+// const arr2 = arr1.flat()
+
+// let result = arr2.reduce(function(sum, elem) {
+
+//     return sum + elem;
+// 	}); 
+
+// console.log(result)
+
