d5affa19bf608b516f7279c0f6eb0f1ca26e7a3a
tkozlovets <tkozlovets@ukr.net>
2021-12-03 18:38:42 +0200

    added tasks 1-6

--- lesson5/js/myhomework.js
+++ lesson5/js/myhomework.js
@@ -37,3 +37,55 @@
 
 // console.log(result)
 
+// task3
+// Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, что оно больше нуля и меньше 10. 
+// Если это так - пусть функция возвращает true, если не так - false.
+
+// function isNumberInRange(x) {
+//     if (x > 0 && x < 10) {
+//     return true;
+//     }
+//     else
+//     return false;
+//     }
+
+// console.log(isNumberInRange(5))
+
+// task4
+// Сделайте функцию isEven() (even - это четный), которая параметром принимает целое число и проверяет: четное оно или нет. 
+// Если четное - пусть функция возвращает true, если нечетное - false.
+
+// function isEven(x) {
+//     if (x%2) {
+//     return false;
+//     }
+//     else
+//     return true;
+//     }
+// console.log(isEven(100))
+
+// task5
+// Дан массив с целыми числами. Создайте из него новый массив, где останутся лежать только четные из этих чисел. 
+// Для этого используйте вспомогательную функцию isEven из предыдущей задачи.
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// let arr1 = arr.filter(function(x) {
+//     if (x%2 === 0)
+//     return x;
+//   });
+//   console.log(arr1)
+
+// task6
+// Сделайте функцию getDivisors, которая параметром принимает число и возвращает массив его делителей 
+// (чисел, на которое делится данное число).
+
+
+// function getDivisors(x) {
+//     let arr = []
+//     for(i = 1; i <= x; i++) 
+//     if (x%i===0)
+//    arr.push(i);   
+//    return arr;
+//  }
+// console.log(getDivisors(6))
+
