30f2843da4ceefbf3b2825a1f0fc3a9806aafbbe
tkozlovets <tkozlovets@ukr.net>
2021-12-10 19:49:13 +0200

    added cycles tasks

---
+++ lesson6/js/cycles.js
@@ -0,0 +1,115 @@
+
+// task1
+
+// let arr = []
+// for(i=0; i<=300; i++)
+// {
+//     arr.push(i)
+// } 
+
+// console.log(arr)
+
+// task2
+// 4, 7, 10, 13
+// let arr = []
+// for(i=4; i<=13; i+=3)
+// {
+//     arr.push(i)
+// } 
+
+// console.log(arr)
+
+// task3
+
+// let arr = []
+// for(i=0; i<=654; i++)
+// {
+//     arr.push(i)
+// } 
+
+// console.log(arr.reverse())
+
+// task4
+1983-2017
+// let arr = []
+// for(i=1983; i<=2017; i++)
+// {
+//     arr.push(i)
+// } 
+
+// console.log(arr)
+
+// task5
+// -4 до 100
+// let arr = []
+// for(i=-4; i<=100; i++)
+// {
+//     arr.push(i)
+// } 
+
+// console.log(arr)
+
+// task6
+// 1-9 на 7
+
+// let arr = []
+
+// for (let i = 1; i <= 9; i++){  
+     
+//     arr.push(i + "*" + 7 + "=" +(i*7));
+
+ 
+// } 
+// console.log(arr)
+
+// task8
+// 0-100 Sum
+
+// function numberSum(N) {
+//     let total = 0;
+//       for(i = 0; i <= N; i++){
+//         total += i;
+//       }
+//       return total;
+//   }
+//   console.log(numberSum(100))
+
+// task9
+// 1-50 multiply
+
+// function numberMult(N) {
+//     let total = 1;
+//       for(i = 1; i <= N; i++){
+//         total *= i;
+//       }
+//       return total;
+//   }
+//   console.log(numberMult(50))
+
+// task10
+
+// let a = +prompt('введите длину массива')
+
+// let arr = []
+// a = arr.length
+
+// for(i=0; i<=a; i++)
+// { arr.push(arr[i - 2] + 'x') };
+// alert(arr) 
+
+// task13
+// let array = [1, 2, 3, 4]
+// for (i = array.length-1; i>=0 ; i--) {
+//     console.log(array[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
